1d6e7db63c47baf1dbbe4f32955d89ccf342e3c8
Yanis Khelifi <97352917+yaniskhelifi00@users.noreply.github.com>
2025-11-03 14:50:49 +0100

    Update firebaseAdmin.js

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,9 +1,14 @@
+// src/utils/firebaseAdmin.js
 import admin from "firebase-admin";
 
+// Initialize Firebase Admin only once
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.applicationDefault(),
   });
 }
 
-export default admin;
+// Get Firestore instance
+const db = admin.firestore();
+
+export { admin, db };
